1b939f4e24ea3fa8175292d48081a2cc45921ba3
wnkbll <arzhevitin.g@gmail.com>
2024-01-16 22:56:45 +0500

    utils.environment:added type hints

--- src/utils/environment.py
+++ src/utils/environment.py
@@ -6,10 +6,10 @@ class Environment:
     def __init__(self, path: str | os.PathLike):
         dotenv.load_dotenv(path)
 
-        self.api_id = int(os.getenv("API_ID"))
-        self.api_hash = os.getenv("API_HASH")
-        self.token = os.getenv("TOKEN")
-        self.phone = os.getenv("PHONE")
+        self.api_id: int = int(os.getenv("API_ID"))
+        self.api_hash: str = os.getenv("API_HASH")
+        self.token: str = os.getenv("TOKEN")
+        self.phone: str = os.getenv("PHONE")
 
 
 environment = Environment(".env")
