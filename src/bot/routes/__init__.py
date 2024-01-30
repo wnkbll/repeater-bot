ead33f881d2937b3fb2f92b6e7f10a62d6b96cb2
wnkbll <arzhevitin.g@gmail.com>
2024-01-30 23:09:41 +0500

    src.bot.router: initial help commit

--- src/bot/routes/__init__.py
+++ src/bot/routes/__init__.py
@@ -1,6 +1,6 @@
-from src.bot.routes import start, add, ls, edit, remove
+from src.bot.routes import start, add, ls, edit, remove, help
 
-routers = [start.router, add.router, ls.router, edit.router, remove.router]
+routers = [start.router, add.router, ls.router, edit.router, remove.router, help.router]
 
 __all__ = [
     "routers"
