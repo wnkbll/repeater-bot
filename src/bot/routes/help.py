fd482c2e123c4df74cd191a049767e5df258d3a1
wnkbll <arzhevitin.g@gmail.com>
2024-01-31 10:02:18 +0500

    src.bot.routes.help: remove args checking because it's useless

--- src/bot/routes/help.py
+++ src/bot/routes/help.py
@@ -1,15 +1,12 @@
 from aiogram import Router, F
-from aiogram.filters import Command, CommandObject
+from aiogram.filters import Command
 from aiogram.filters.callback_data import CallbackQuery
 from aiogram.types import Message
-
-from loguru import logger
 
 from src.bot.callbacks import HelpCallback
 from src.bot.keyboards import HelpKeyboard
 from src.bot.filters import WhiteListFilter
 from src.utils import Globals
-from src.lang import STRINGS
 
 lang = Globals.lang
 
@@ -20,10 +17,10 @@ router.message.filter(WhiteListFilter())
 @router.callback_query(HelpCallback.filter(F.action == "help"))
 async def _help_callback(query: CallbackQuery, callback_data: HelpCallback):
     commands = {
-        "add": "```add\n/add [arg]```",
-        "ls": "```ls\n/ls [arg]```",
-        "edit": "```edit\n/edit [arg]```",
-        "remove": "```remove\n/remove [arg]```"
+        "add": "```add\n/add [arg]. Arg = [post, chat, пост, чат]```",
+        "ls": "```ls\n/ls [arg]. Arg = [post, chat, sleep, пост, чат, сон]```",
+        "edit": "```edit\n/edit [arg]. Arg = [post, chat, sleep, пост, чат, сон]```",
+        "remove": "```remove\n/remove [arg]. Arg = [post, chat, пост, чат]```"
     }
 
     builder = HelpKeyboard().builder
@@ -31,11 +28,6 @@ async def _help_callback(query: CallbackQuery, callback_data: HelpCallback):
 
 
 @router.message(Command("help", "помощь"))
-async def _help(message: Message, command: CommandObject):
-    if command.args is not None:
-        await message.answer(STRINGS[lang]["unexpected_args"])
-        logger.warning(STRINGS["debug"]["unexpected_args"].format(username=message.from_user.username))
-        return None
-
+async def _help(message: Message):
     builder = HelpKeyboard().builder
     await message.answer("Выберите команду", reply_markup=builder.as_markup())
