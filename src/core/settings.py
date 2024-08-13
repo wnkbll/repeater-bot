920817175f1a0677aa4523ad8b44f87e0720e9e0
wnkbll <arzhevitin.g@gmail.com>
2024-08-13 14:34:22 +0500

    Update dependencies, update settings, update docker-compose.yml

--- src/core/settings.py
+++ src/core/settings.py
@@ -1,4 +1,6 @@
-from pydantic import Field
+from functools import lru_cache
+
+from pydantic import Field, RedisDsn, BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from src.core.paths import ENV_PATH
@@ -7,10 +9,32 @@ settings_config_dict = SettingsConfigDict(
     env_file=ENV_PATH, env_file_encoding='utf-8', validate_default=False, extra="ignore",
 )
 
-class Settings(BaseSettings):
+
+class TelegramSettings(BaseSettings):
     model_config = settings_config_dict
 
     api_id: int = Field(validation_alias="API_ID")
     api_hash: str = Field(validation_alias="API_HASH")
     token: str = Field(validation_alias="TOKEN")
     phone: str = Field(validation_alias="PHONE")
+
+
+class RedisSettings(BaseSettings):
+    model_config = settings_config_dict
+
+    host: str = Field(validation_alias="REDIS_HOST")
+    port: str = Field(validation_alias="REDIS_PORT")
+
+
+class Settings(BaseModel):
+    telegram: TelegramSettings = TelegramSettings()
+    redis: RedisSettings = RedisSettings()
+
+    @property
+    def redis_dsn(self) -> RedisDsn:
+        return f"redis://{self.host}:{self.port}"
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
