8583e05f1decd14116815bc419d54eb42e652d49
wnkbll <arzhevitin.g@gmail.com>
2024-08-13 14:34:31 +0500

    Update dependencies, update settings, update docker-compose.yml

---
+++ src/db/redis.py
@@ -0,0 +1,58 @@
+from loguru import logger
+from redis import asyncio as aioredis
+
+from src.core.settings import get_settings
+
+KeyType = bytes | str | memoryview
+ValueType = bytes | str | memoryview | int | float
+
+class Redis:
+    @staticmethod
+    async def set(*, key: KeyType, value: ValueType) -> None:
+        settings = get_settings()
+        redis = aioredis.from_url(settings.redis_dsn)
+
+        async with redis.client() as connection:
+            await connection.set(key, value)
+
+        logger.info(f"Key {key} was set")
+
+    @staticmethod
+    async def get(*, key: KeyType) -> ValueType:
+        settings = get_settings()
+        redis = aioredis.from_url(settings.redis_dsn)
+
+        async with redis.client() as connection:
+            encoded = await connection.get(key)
+
+        logger.info(f"Key {key} was read")
+
+        return encoded
+
+    @staticmethod
+    async def delete(*, key: KeyType) -> None:
+        settings = get_settings()
+        redis = aioredis.from_url(settings.redis_dsn)
+
+        async with redis.client() as connection:
+            await connection.delete(key)
+
+        logger.info(f"Key {key} was deleted")
+
+    @staticmethod
+    async def exists(*, key: KeyType) -> bool:
+        settings = get_settings()
+        redis = aioredis.from_url(settings.redis_dsn)
+
+        async with redis.client() as connection:
+            return bool(await connection.exists(key))
+
+    @staticmethod
+    async def flush_all() -> None:
+        settings = get_settings()
+        redis = aioredis.from_url(settings.redis_dsn)
+
+        async with redis.client() as connection:
+            await connection.flushall()
+
+        logger.info("Redis database was flushed")
