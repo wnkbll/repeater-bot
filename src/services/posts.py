225f48898a17dd4dd17d97b0913fcbc3ee7d2622
wnkbll <arzhevitin.g@gmail.com>
2024-08-15 08:24:20 +0500

    Initial commit for service module

---
+++ src/services/posts.py
@@ -0,0 +1,18 @@
+from src.models import Post
+
+
+class PostsService:
+    def __init__(self):
+        self.posts: list[Post] = []
+
+    async def create(self) -> None:
+        pass
+
+    async def get(self) -> None:
+        pass
+
+    async def update(self) -> None:
+        pass
+
+    async def delete(self) -> None:
+        pass
