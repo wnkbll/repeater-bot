a86ad8e668b7caf03b002b15689617c33c4bff21
wnkbll <arzhevitin.g@gmail.com>
2024-08-16 23:45:07 +0500

    Update paths

--- src/core/paths.py
+++ src/core/paths.py
@@ -3,6 +3,7 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parent.parent.parent
 
 ENV_PATH = ROOT_DIR.joinpath(".env")
+IMAGE_DIR = ROOT_DIR.joinpath("data")
 LOGGING_DIR = ROOT_DIR.joinpath("logs")
 CONFIG_PATH = ROOT_DIR.joinpath("config.json")
 SESSION_PATH = ROOT_DIR.joinpath("session/client")
